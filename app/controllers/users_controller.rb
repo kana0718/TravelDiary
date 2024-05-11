7d1e69150bdca8c68c47fbe8a7b201e484e7bd39
kana0718 <g.k718g.s.love@gmail.com>
2024-05-11 11:04:55 +0000

    Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :ensure_guest_user, only: [:edit]
   def index
     @user = User.all
     @user = current_user.id
@@ -35,6 +36,13 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     unless user.id == current_user.id
       redirect_to user_path(current_user)
+    end
+  end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.email == "guest@example.com"
+      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,11 @@ end
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
+  
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
 
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
