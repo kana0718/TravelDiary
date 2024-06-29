09e9342fae6f92ec5832e76eb051f2540a0e19e7
kana0718 <g.k718g.s.love@gmail.com>
2024-06-29 07:06:45 +0000

    完成3

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   resource :home, only: [] do
-    get :about
+    get "about"
   end
   get "search" => "searches#search"
   resources :favorites, only: [:index]
